b305bf3f230083779918202c1376d47dc5d492ab
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-01-04 23:11:05 +0700

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,31 @@
+'''
+class SampleInfo():
+    this is a sample class <== # docstring
+    x = 20
+    y = 30
+    def method_1(self):
+        print('m1 of class sample')
+        print(SampleInfo.x)
+SampleInfo().method_1()
+print(SampleInfo.y)000
+print(SampleInfo.__doc__)
+print(help(SampleInfo))
+print(dir(SampleInfo))
+
+class test:
+    def sample(self, name = 'Guest'):
+        print('hello',name,'good morning')
+t = test()
+t.sample('pratik')
+t.sample()
+
+name = 'pratik'
+age = 24
+def sample():
+    global name
+    global age
+#def display():
+    #print(name, age)
+sample()
+display()
+'''
