0ea86076214908cd85bca6e3f5073fe0668dcf5e
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-01-19 20:53:07 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -29,4 +29,35 @@ def sample():
     #print(name, age)
 sample()
 display()
+======================================================================================================================
+# Write a Python program to create an instance of a specified class and display the namespace of the said instance
+class sample:
+    def __init__(self,name,marks):
+        self.n = name
+        self.m = marks
 
+s = sample('pratik',88)
+
+print(s.n ,'and',s.m)
+
+class Circle:
+    def __init__(self,a,b,r):
+        self.a = a
+        self.b = b
+        self.r = r
+    def area(self):
+        print('area:',(3.142)*float((self.r)**2))
+    def perimeter(self):
+        print('perimeter:', 2*3.142*self.r)
+    def testbelongs(self):
+        x = 25
+        y = 10
+        if ((x-self.a)*(x-self.a))+((y-self.b)*(y-self.b)) <= (self.r)*(self.r):
+            print('inside')
+        else:
+            print('outside')
+#x,y = int(input('enter two point to check circle: '))
+c = Circle(0,0,40)
+c.area()
+c.perimeter()
+c.testbelongs()
