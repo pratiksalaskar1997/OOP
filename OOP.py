012132d524263886d40a9e5651a7dd5065a4e861
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-02-02 00:51:29 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,4 +1,18 @@
 
+#SYNTAX OF A CLASS:
+class SampleInfo():
+    this is a sample class <== # docstring
+    x = 20
+    y = 30
+    def method_1(self):
+        print('m1 of class sample')
+        print(SampleInfo.x)
+SampleInfo().method_1()
+print(SampleInfo.y)000
+print(SampleInfo.__doc__)
+print(help(SampleInfo))
+print(dir(SampleInfo))
+
 class test:
     def sample(self, name = 'Guest'):
         print('hello',name,'good morning')
